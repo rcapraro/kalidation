cb7658b05a70dd036d9f9fd9600b353d3af7378a
Romain Vetrano <rvetrano@sada.fr>
2018-11-15 14:43:09 +0100

    add validation for subset

--- src/test/kotlin/IterableValidationTest.kt
+++ src/test/kotlin/IterableValidationTest.kt
@@ -1,6 +1,7 @@
 import com.capraro.kalidation.constraints.function.notEmpty
 import com.capraro.kalidation.constraints.function.notNull
 import com.capraro.kalidation.constraints.function.size
+import com.capraro.kalidation.constraints.function.subSetOf
 import com.capraro.kalidation.dsl.constraints
 import com.capraro.kalidation.dsl.property
 import com.capraro.kalidation.dsl.validationSpec
@@ -36,6 +37,90 @@ class IterableValidationTest {
                 },
                 { fail("The validation should not be valid") }
         )
+
+    }
+
+    @Test
+    fun `test validation of Iterable fields is not subset of oher list`() {
+        val spec = validationSpec {
+            constraints<IterableClass> {
+                property(IterableClass::listField) {
+                    subSetOf("GREEN", "WHITE", "RED")
+                }
+            }
+        }
+        val dslTest = IterableClass(listOf("one", "two"))
+
+        val validated = spec.validate(dslTest)
+
+        assert(validated.isInvalid)
+
+        validated.fold(
+                {
+                    Assertions.assertThat(it).extracting("fieldName")
+                            .containsExactly("listField")
+                },
+                { fail("The validation should not be valid") }
+        )
+
+    }
+
+    @Test
+    fun `test validation of Iterable fields is partial subset of oher list`() {
+        val spec = validationSpec {
+            constraints<IterableClass> {
+                property(IterableClass::listField) {
+                    subSetOf("GREEN", "WHITE", "RED")
+                }
+            }
+        }
+        val dslTest = IterableClass(listOf("GREEN", "two"))
+
+        val validated = spec.validate(dslTest)
+
+        assert(validated.isInvalid)
+
+        validated.fold(
+                {
+                    Assertions.assertThat(it).extracting("fieldName")
+                            .containsExactly("listField")
+                },
+                { fail("The validation should not be valid") }
+        )
+
+    }
+
+    @Test
+    fun `test validation of Iterable fields is subset of oher list`() {
+        val spec = validationSpec {
+            constraints<IterableClass> {
+                property(IterableClass::listField) {
+                    subSetOf("GREEN", "WHITE", "RED")
+                }
+            }
+        }
+        val dslTest = IterableClass(listOf("GREEN", "WHITE"))
+
+        val validated = spec.validate(dslTest)
+
+        assert(validated.isValid)
+
+    }
+
+    @Test
+    fun `test validation of Iterable fields empty is subset of oher list empty`() {
+        val spec = validationSpec {
+            constraints<IterableClass> {
+                property(IterableClass::listField) {
+                    subSetOf()
+                }
+            }
+        }
+        val dslTest = IterableClass(listOf())
+
+        val validated = spec.validate(dslTest)
+
+        assert(validated.isValid)
 
     }
 }
