0d6c3558f3da29f2c5a0f8ac168d1c60415ca68c
Richard Capraro <richard.capraro@gmail.com>
2018-11-17 15:21:05 +0100

    More validation by script langages: jexl and groovy

--- src/test/kotlin/ScriptValidationTest.kt
+++ src/test/kotlin/ScriptValidationTest.kt
@@ -9,12 +9,14 @@ import org.junit.jupiter.api.fail
 
 private class MyClass(val field: MyInnerClass)
 
-data class MyInnerClass(val innerField1: String, val innerField2: String)
+data class MyInnerClass(val innerField1: String, val innerField2: String) {
+    fun validate(): Boolean = innerField1.length > innerField2.length
+}
 
 class ScriptValidationTest {
 
     @Test
-    fun `test validation by script should fail`() {
+    fun `test validation by javascript script should fail`() {
         val spec = validationSpec {
             constraints<MyClass> {
                 property(MyClass::field) {
@@ -40,7 +42,7 @@ class ScriptValidationTest {
     }
 
     @Test
-    fun `test validation by script should succeed`() {
+    fun `test validation by javascript script should succeed`() {
         val spec = validationSpec {
             constraints<MyClass> {
                 property(MyClass::field) {
@@ -57,4 +59,114 @@ class ScriptValidationTest {
         assert(validated.isValid)
 
     }
+
+    @Test
+    fun `test validation by javascript script calling a function should succeed`() {
+        val spec = validationSpec {
+            constraints<MyClass> {
+                property(MyClass::field) {
+                    notNull()
+                    validByScript("javascript", "field.validate()", "field")
+                }
+            }
+        }
+
+        val dslTest = MyClass(MyInnerClass("foobar", "bar"))
+
+        val validated = spec.validate(dslTest)
+
+        assert(validated.isValid)
+
+    }
+
+    @Test
+    fun `test validation by jexl script should fail`() {
+        val spec = validationSpec {
+            constraints<MyClass> {
+                property(MyClass::field) {
+                    notNull()
+                    validByScript("jexl", "field.innerField1.size() > field.innerField2.size()", "field")
+                }
+            }
+        }
+
+        val dslTest = MyClass(MyInnerClass("foo", "foobar"))
+
+        val validated = spec.validate(dslTest)
+
+        println(validated)
+
+        validated.fold(
+                {
+                    Assertions.assertThat(it).extracting("fieldName").containsExactly("field")
+                },
+                { fail("The validation should not be valid") }
+        )
+
+    }
+
+    @Test
+    fun `test validation by jexl script calling a function should succeed`() {
+        val spec = validationSpec {
+            constraints<MyClass> {
+                property(MyClass::field) {
+                    notNull()
+                    validByScript("jexl", "field.validate()", "field")
+                }
+            }
+        }
+
+        val dslTest = MyClass(MyInnerClass("foobar", "bar"))
+
+        val validated = spec.validate(dslTest)
+
+        assert(validated.isValid)
+
+    }
+
+    @Test
+    fun `test validation by groovy script should fail`() {
+        val spec = validationSpec {
+            constraints<MyClass> {
+                property(MyClass::field) {
+                    notNull()
+                    validByScript("groovy", "field.innerField1.size() > field.innerField2.size()", "field")
+                }
+            }
+        }
+
+        val dslTest = MyClass(MyInnerClass("foo", "foobar"))
+
+        val validated = spec.validate(dslTest)
+
+        println(validated)
+
+        validated.fold(
+                {
+                    Assertions.assertThat(it).extracting("fieldName").containsExactly("field")
+                },
+                { fail("The validation should not be valid") }
+        )
+
+    }
+
+    @Test
+    fun `test validation by groovy script calling a function should succeed`() {
+        val spec = validationSpec {
+            constraints<MyClass> {
+                property(MyClass::field) {
+                    notNull()
+                    validByScript("groovy", "field.validate()", "field")
+                }
+            }
+        }
+
+        val dslTest = MyClass(MyInnerClass("foobar", "bar"))
+
+        val validated = spec.validate(dslTest)
+
+        assert(validated.isValid)
+
+    }
+
 }
