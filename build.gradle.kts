5fc04a31d6f15104c0219da0d08ab0f44ca94928
Richard Capraro <richard.capraro@gmail.com>
2021-04-11 21:20:48 +0200

    chore: Add publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,15 +24,15 @@ val junitJupiterVersion = "5.7.1"
 val arrowVersion = "0.11.0"
 val hibernateValidatorVersion = "6.2.0.Final"
 val javaxValidationVersion = "2.0.1.Final"
-val libphonenumberVersion = "8.12.21"
+val libPhoneNumberVersion = "8.12.21"
 val groovyVersion = "3.0.7"
-val assertJVersiom = "3.19.0"
+val assertJVersion = "3.19.0"
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("javax.validation:validation-api:$javaxValidationVersion")
     implementation("org.hibernate:hibernate-validator:$hibernateValidatorVersion")
-    implementation("com.googlecode.libphonenumber:libphonenumber:$libphonenumberVersion")
+    implementation("com.googlecode.libphonenumber:libphonenumber:$libPhoneNumberVersion")
     implementation("org.glassfish:javax.el:3.0.1-b12")
     implementation("org.apache.commons:commons-jexl3:3.1")
     implementation("org.codehaus.groovy:groovy-jsr223:$groovyVersion")
@@ -40,7 +40,7 @@ dependencies {
     implementation("io.arrow-kt:arrow-validation:$arrowVersion")
     testImplementation(platform("org.junit:junit-bom:$junitJupiterVersion"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    testCompile("org.assertj:assertj-core:$assertJVersiom")
+    testCompile("org.assertj:assertj-core:$assertJVersion")
 }
 
 tasks.withType<KotlinCompile> {
@@ -64,4 +64,50 @@ jacoco {
 
 ktlint {
     version.set("0.41.0")
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/rcapraro/kalidation")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.capraro"
+            artifactId = "kalidation"
+            version = project.version.toString()
+
+            from(components["java"])
+
+            pom {
+                name.set("Kalidation")
+                description.set("JSR380 Validation DSL in Kotlin")
+                url.set("https://github.com/rcapraro/kalidation")
+                licenses {
+                    license {
+                        name.set("The MIT license")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("rcapraro")
+                        name.set("Richard Capraro")
+                        email.set("richard.capraro@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git@github.com:rcapraro/kalidation.git")
+                    developerConnection.set("scm:git:git@github.com:rcapraro/kalidation.git")
+                    url.set("https://github.com/rcapraro/kalidation")
+                }
+            }
+        }
+    }
 }
